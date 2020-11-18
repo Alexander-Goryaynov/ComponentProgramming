8e29cd560f746a7b465ed4d630106284f603b42d
Alexander-Goryaynov <gor.alex595@gmail.com>
2020-11-18 23:28:25 +0400

    Lab 6 pattern added

---
+++ PatternLibrary/StrategyPattern/Literature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternLibrary.StrategyPattern
+{
+    class Literature
+    {
+        public IReadable ReadStrategy { get; set; }
+        public Literature(IReadable readStrategy)
+        {
+            ReadStrategy = readStrategy;
+        }
+        public string Read()
+        {
+            return ReadStrategy.Read();
+        }
+    }
+}
